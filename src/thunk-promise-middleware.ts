d3ecd6339a679b3da4ed37cbc99fef032abd931e
nanovazquez <marianodvazquez@gmail.com>
2018-05-26 19:59:35 -0300

    docs: Minor update

--- src/thunk-promise-middleware.ts
+++ src/thunk-promise-middleware.ts
@@ -11,14 +11,14 @@ function createThunkPromiseMiddleware(extraArguments: object = {}): any {
       return action({ dispatch, getState, ...extraArguments });
     }
 
-    // If action is a FSA with a thenable as payload,
+    // If action is an FSA with a thenable as payload,
     // wait for the result and then dispatch it
     if (isFSA(action) && isThenable(action.payload)) {
       return dispatchPromiseResult(action.payload, action, dispatch);
     }
 
     // If action is an FSA with a thunk as payload,
-    // execute it and then wait for the result (if it's a thenable)
+    // execute it and then wait for the result if it's a thenable
     if (isFSA(action) && isFunction(action.payload)) {
       const payloadResult = action.payload({ dispatch, getState, ...extraArguments });
       if (payloadResult && isThenable(payloadResult)) {
