bff62e964866831495aa4c446f2e7a1a8ea7e197
nanovazquez <marianodvazquez@gmail.com>
2018-05-26 19:56:46 -0300

    chore(example): Bump versions

--- examples/react-todo-list/src/index.js
+++ examples/react-todo-list/src/index.js
@@ -9,7 +9,7 @@ import './index.css';
 
 // In a real app, the next line should be:
 // import thunkPromiseMiddleware from 'redux-thunk-promise';
-import thunkPromiseMiddleware from '../../../dist';
+import thunkPromiseMiddleware from '../../../lib';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // eslint-disable-line
 const store = createStore(
